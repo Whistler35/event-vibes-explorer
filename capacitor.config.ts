2006efe8cc7b3e0af3600fb9fd4c980f62e17b36
Benjamin Maxwald <benjamin.maxwald@MacBook-Air-von-Benjamin-2.local>
2026-05-09 23:14:53 +0200

    Capacitor iOS Setup

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.19807b4840c04e949e68b82e61ddb804',
-  appName: 'event-vibes-explorer',
+  appId: 'com.evendle.app',
+  appName: 'EVENDLE',
   webDir: 'dist',
-  server: {
-    url: 'https://19807b48-40c0-4e94-9e68-b82e61ddb804.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
   plugins: {
     SplashScreen: {
       launchShowDuration: 0
